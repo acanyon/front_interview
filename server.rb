68fada32fb12a380a77ec991f39a8d7c7f21de05
Amanda Canyon <acanyon@gmail.com>
2017-02-08 23:01:57 -0800

    using ruby style hash access

--- server.rb
+++ server.rb
@@ -30,8 +30,8 @@ end
 
 post '/github_hook' do
   response = JSON.parse request.body.string
-  sender = {handle: response.sender.login}
-  subject = "[#{response.head.repo.name}] #{response.pull_request.title}"
+  sender = {handle: response['sender']['login']}
+  subject = "[#{response['head']['repo']['name']}] #{response['pull_request']['title']}"
   body = "something changed"
   create_front_message({name: 'github', handle: 'github'}, subject, request.body.string)
 
