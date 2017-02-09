9d886d81952d0fccc56558ed75faebe0295d30b9
Amanda Canyon <acanyon@gmail.com>
2017-02-08 22:40:26 -0800

    to json body

--- server.rb
+++ server.rb
@@ -29,6 +29,6 @@ get '/' do
 end
 
 post '/github_hook' do
-  create_front_message({name: 'github', handle: 'github'}, 'pull request', request.body)
+  create_front_message({name: 'github', handle: 'github'}, 'pull request', request.body.to_json)
 end
 
